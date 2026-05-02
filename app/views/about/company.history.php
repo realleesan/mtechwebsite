a4c93d73fbfe92bc3f453e564d8f8eb5ad40b378
realleesan <baominhkpkp@gmail.com>
2026-05-02 20:46:48 +0700

    a

--- app/views/about/company.history.php
+++ app/views/about/company.history.php
@@ -9,46 +9,61 @@
         <div class="media about_history">
             <img src="https://shtheme.com/demosd/wokrate/wp-content/uploads/2019/12/about_img-1.jpg" alt="about-img">
             <div class="media-body">
-                <h2 class="f_600 f_size_32 title_color d-inline-block">About Company</h2>
+                <h2 class="f_600 f_size_32 title_color d-inline-block">Lịch sử hình thành & phát triển</h2>
                 <span class="title_br"></span>
-                <h5 class="f_l f_size_20 title_color f_400">Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved.</h5>
-                <p class="mb_20">sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porroisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. sed quia non numquam qui ratione voluptatem sequi nesciunt.</p>
-                <p class="mb-0">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipiscil  consectetur, adipisci velit. sed quia non numquam qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum.</p>
+                <h5 class="f_l f_size_20 title_color f_400">Công ty Cổ phần Tư vấn Kỹ thuật và Thương mại MTECH - Đối tác tin cậy trong lĩnh vực tư vấn thiết kế và giám sát công trình công nghiệp tại Việt Nam.</h5>
+                <p class="mb_20">MTECH được thành lập với sứ mệnh cung cấp giải pháp tư vấn kỹ thuật toàn diện cho các dự án công nghiệp trọng điểm. Với đội ngũ 25 chuyên gia, thạc sĩ và kỹ sư đa ngành, chúng tôi đã thực hiện hàng trăm dự án lớn trong lĩnh vực xi măng, vôi, thép và năng lượng.</p>
+                <p class="mb-0">Từ những bước đầu tư vấn các trạm nghiền xi măng quy mô nhỏ, MTECH đã phát triển thành đơn vị tư vấn hạng I với khả năng thiết kế và giám sát các công trình công nghiệp quy mô lớn nhất Việt Nam, trở thành đối tác chiến lược của các tập đoàn hàng đầu như Xuân Thành, Long Sơn, Thành Thắng, Xuân Thiện.</p>
             </div>
         </div>
         <div class="history_info mb-50">
             <div class="media history_item">
-                <div class="history_date">1998</div>
+                <div class="history_date">2011</div>
                 <div class="media-body">
-                    <h3 class="f_600 f_p title_color">Launch Company</h3>
-                    <p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it. which of us ever undertakes laborious physical exercise, except to obtain some advantage from it. which of us ever undertakes laborious physical exercise, except to obtain some advantage from it</p>
+                    <h3 class="f_600 f_p title_color">Thành lập Công ty</h3>
+                    <p>Ngày 26/05/2011, Công ty Cổ phần Tư vấn Kỹ thuật và Thương mại MTECH chính thức được thành lập theo Giấy chứng nhận đăng ký doanh nghiệp số 0105330414 do Sở Kế hoạch và Đầu tư thành phố Hà Nội cấp. Đây là dấu mốc khởi đầu cho hành trình phát triển của một đơn vị tư vấn kỹ thuật chuyên nghiệp tại Việt Nam.</p>
                 </div>
             </div>
             <div class="media history_item">
-                <div class="history_date">2001</div>
+                <div class="history_date">2012-2017</div>
                 <div class="media-body">
-                    <h3 class="f_600 f_p title_color">Member Increased in Company 25 +</h3>
-                    <p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it. which of us ever undertakes laborious physical exercise, except to obtain some advantage from it. which of us ever undertakes laborious physical exercise, except to obtain some advantage from it</p>
+                    <h3 class="f_600 f_p title_color">Giai đoạn Khởi đầu & Khẳng định uy tín</h3>
+                    <p>MTECH tập trung vào các dự án công nghiệp nặng, đặc biệt là ngành xi măng và vôi. Các dự án tiêu biểu gồm Trạm nghiền xi măng Đắk Nông (2012), Nhà máy sản xuất vôi chất lượng cao Quảng Bình 1 triệu tấn/năm (2013), và bắt đầu tư vấn tổng thầu cho các hệ thống phát điện nhiệt dư tại NM xi măng Sông Gianh, Quảng Phúc (2017).</p>
                 </div>
             </div>
             <div class="media history_item">
-                <div class="history_date">2005</div>
+                <div class="history_date">2018</div>
                 <div class="media-body">
-                    <h3 class="f_600 f_p title_color">Get Best Service Award</h3>
-                    <p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it. which of us ever undertakes laborious physical exercise, except to obtain some advantage from it. which of us ever undertakes laborious physical exercise, except to obtain some advantage from it</p>
+                    <h3 class="f_600 f_p title_color">Nâng tầm vị thế - Chứng chỉ Hạng I</h3>
+                    <p>Tháng 12/2018, MTECH đạt bước tiến quan trọng khi được cấp Chứng chỉ năng lực hoạt động xây dựng Hạng I cho lĩnh vực thiết kế và giám sát công trình công nghiệp. Đây là chứng chỉ cao nhất trong ngành, khẳng định năng lực thực hiện các dự án quy mô lớn. MTECH trở thành đối tác chiến lược của các tập đoàn lớn như Xuân Thành, Long Sơn, Thành Thắng.</p>
                 </div>
             </div>
             <div class="media history_item">
-                <div class="history_date">2016</div>
+                <div class="history_date">2019-2021</div>
                 <div class="media-body">
-                    <h3 class="f_600 f_p title_color">5 + More Branches Open in City</h3>
-                    <p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it. which of us ever undertakes laborious physical exercise, except to obtain some advantage from it. which of us ever undertakes laborious physical exercise, except to obtain some advantage from it</p>
-                    <div class="history_img">
-                        <img src="https://shtheme.com/demosd/wokrate/wp-content/uploads/2019/12/01.jpg" alt="">
-                    </div>
-                    <div class="history_img">
-                        <img src="https://shtheme.com/demosd/wokrate/wp-content/uploads/2019/12/02.jpg" alt="">
-                    </div>
+                    <h3 class="f_600 f_p title_color">Mở rộng thị trường & Các dự án trọng điểm</h3>
+                    <p>Giai đoạn này ghi dấu ấn với các dự án xi măng quy mô lớn: NM xi măng Xuân Thành Line 3 (5 triệu tấn/năm), NM xi măng Long Sơn Line 3 & 4, NM xi măng Thành Thắng Line 3,4&5, NM xi măng Đại Dương Line 2. MTECH cũng mở rộng sang lĩnh vực vôi công nghiệp và xử lý tro bay.</p>
+                </div>
+            </div>
+            <div class="media history_item">
+                <div class="history_date">2023</div>
+                <div class="media-body">
+                    <h3 class="f_600 f_p title_color">Cấp phép hoạt động Điện lực</h3>
+                    <p>Ngày 25/08/2023, MTECH được cấp Giấy phép hoạt động điện lực số 43/GP-SCT do Sở Công Thương thành phố Hà Nội cấp, mở rộng khả năng tư vấn trong lĩnh vực năng lượng. Công ty tham gia các dự án phát điện nhiệt dư quy mô lớn tại Vicem Hải Phòng (6 MW) và Nghi Sơn (18 MW).</p>
+                </div>
+            </div>
+            <div class="media history_item">
+                <div class="history_date">2024</div>
+                <div class="media-body">
+                    <h3 class="f_600 f_p title_color">Đa dạng hóa lĩnh vực Đầu tư</h3>
+                    <p>Công ty mở rộng mạnh mẽ sang lĩnh vực luyện kim và nông nghiệp ứng dụng công nghệ cao. Các dự án trọng điểm bao gồm Nhà máy luyện cán thép DST Nghi Sơn (1,2 triệu tấn/năm), các khu chăn nuôi công nghệ cao tại Thanh Hóa. Tháng 04/2024, MTECH chính thức đăng ký địa điểm kinh doanh mới tại tòa nhà 227 phố Nguyễn Ngọc Nại.</p>
+                </div>
+            </div>
+            <div class="media history_item">
+                <div class="history_date">2025</div>
+                <div class="media-body">
+                    <h3 class="f_600 f_p title_color">Tầm nhìn tương lai & Dự án Xanh</h3>
+                    <p>Tháng 05/2025, MTECH thực hiện thay đổi đăng ký kinh doanh lần thứ 5, khẳng định quy mô phát triển ngày càng lớn mạnh. Công ty hướng tới các dự án bền vững và quy mô quốc gia: Nhà máy Thép xanh Xuân Thiện Nam Định (7,5 triệu tấn/năm), các khu nông nghiệp tập trung tại Đồng Nai, Hòa Bình với quy mô hàng ngàn héc-ta.</p>
                 </div>
             </div>
         </div>
